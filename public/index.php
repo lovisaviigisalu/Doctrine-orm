2066e69ca54a9ffd70ce61369a8a8fb33afc6cee
lovisa viigisalu <lovisa.viigisalu@gmail.com>
2024-10-01 20:56:18 +0300

    Data fetched from the database, displaying one article

--- public/index.php
+++ public/index.php
@@ -11,7 +11,7 @@ require('../vendor/autoload.php');
 $container = new Container();
 
 $container->set('db', function(){
-    return DatabaseFactory::create;
+    return DatabaseFactory::create();
 });
 
 $container->set('templating', function() {
@@ -28,6 +28,7 @@ $app->get('/', '\App\Controller\DefaultController:homepage');
 $app->get('/admin', '\App\Controller\AdminController:view');
 $app->any('/admin/create', '\App\Controller\AdminController:create');
 $app->any('/admin/{id}', '\App\Controller\AdminController:edit');
+$app->get('/article/{slug}', '\App\Controller\ArticleController:view');
 
 // finish
 $app->run();
---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class ArticleController extends Controller
+{
+    public function view(Request $request, Response $response)
+    {
+    
+        $article = $this->ci->get('db')->find('App\Entity\Article', 1);
+        
+        return $this->renderPage($response, 'article.html', [ 
+            'article' => $article
+        ]);
+    }
+}
