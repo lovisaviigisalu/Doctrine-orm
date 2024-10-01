11fd31687d779b982dfaa92dcd2fcd75699dc580
lovisa viigisalu <lovisa.viigisalu@gmail.com>
2024-10-01 22:52:33 +0300

    Data retrieved and displayed for a single article using Query Builder

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -11,6 +11,23 @@ class ArticleController extends Controller
 {
     public function view(Request $request, Response $response, $args = [])
     {
+        $qb = $this->ci->get('db')->createQueryBuilder();
+
+        $qb->select('a')
+            ->from('App\Entity\Article', 'a')
+            ->where('a.slug = :slug')
+            ->setParameter('slug', $args['slug']);
+
+        $query = $qb->getQuery();
+
+        $article = $query->getSingleResult();
+
+        return $this->renderPage($response, 'article.html', [ 
+            'article' => $article
+        ]);
+    }
+    public function viewRP(Request $request, Response $response, $args = [])
+    {
     
         $article = $this->ci->get('db')->getRepository('App\Entity\Article')->findOneBy([
                 'slug' => $args['slug']
@@ -24,7 +41,6 @@ class ArticleController extends Controller
             'article' => $article
         ]);
     }
-
     public function viewPK(Request $request, Response $response)
     {
     
